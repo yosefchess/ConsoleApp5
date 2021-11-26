ea544d4720cb3272b3dffb4fbc235470f86a93a6
IT Shop <IT Shop@DESKTOP-BIH0M27>
2021-11-27 00:51:00 +0200

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> Yossef = new LinkedList<string>();
+
+            Yossef.AddFirst("ALOOO");
+            Yossef.AddFirst("ALOO");
+            Yossef.AddLast("ALOOOO");
+            Yossef.AddLast("Done");
+
+            Console.WriteLine("The linked List Before Reversing:  ");
+
+            foreach (var item in Yossef)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("The linked List After Reversing: ");
+
+            foreach (var item in Yossef.Reverse())
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
